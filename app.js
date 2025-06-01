9551f0fc5fed9de153dd4cc4f829f8a316bcc16a
Nandita Sarwate <sarwatenandita@gmail.com>
2025-06-01 16:49:39 +0530

    Initial commit: Flicktionary movie search app

---
+++ app.js
@@ -0,0 +1,23 @@
+        const form = document.querySelector('#searchForm');
+        const resultsContainer = document.querySelector('#resultsContainer');
+        form.addEventListener('submit', async function(e) {
+            e.preventDefault();
+             resultsContainer.innerHTML = '';
+
+            const userSearch = form.elements.query.value;
+            form.elements.query.value = '';
+            const res = await axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=b74ffc8d&s=${userSearch}`);
+            makeImages(res.data.Search);
+            console.log(res.data);
+                                
+        })      
+
+        
+    const makeImages = (movies) => {
+        for (let result of movies ) {
+        const img = document.createElement('IMG');
+            img.src = result.Poster;
+            resultsContainer.appendChild(img);
+            }
+    }
+        
